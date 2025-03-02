ba0ccbf613d07bd630eb68e1354fabe424565cbe
Jon <unseldjonas0@gmail.com>
2025-03-02 23:57:51 +0100

    Added target tests

--- bridge_tester/client/targetTest.lua
+++ bridge_tester/client/targetTest.lua
@@ -1,83 +1,159 @@
+local model = 'prop_protest_table_01'
+
+local options = {
+    {
+        label = 'Test',
+        name = 'bridge_tester_test_one',
+        icon = 'fas fa-clipboard',
+        canInteract = function(entity, distance)
+            return true
+        end,
+        onInteract = function(entity)
+            exports.it_bridge:SendNotification('bridge_tester', 'You tested a target interaction', 'Success', 5000)
+        end,
+        distance = 1.5
+    },
+    {
+        label = 'Test 2',
+        name = 'bridge_tester_test_two',
+        icon = 'fas fa-clipboard',
+        canInteract = function(entity, distance)
+            return true
+        end,
+        onInteract = function(entity)
+            exports.it_bridge:SendNotification('bridge_tester', 'You tested a target interaction', 'Success', 5000)
+        end,
+        distance = 1.5
+    },
+}
+
+
 RegisterNetEvent('bridge_tester:client:targetZoneTest', function()
-end)
+    lib.requestModel(model)
 
-RegisterNetEvent('bridge_tester:client:modelTargetTest', function()
+    local min, max = GetModelDimensions(model)
+    -- Calculate prop dimensions
+    local size = vector3(max.x - min.x, max.y - min.y, max.z - min.z)
 
-    lib.requestModel('prop_protest_table_01')
+    -- Get coords infront of player
     local coords = GetEntityCoords(PlayerPedId())
-    local _ , groundZ = GetGroundZFor_3dCoord(coords.x, coords.y, coords.z, false)
-    local obj = CreateObject('prop_protest_table_01', coords.x, coords.y, groundZ, true, true, true)
+    local forward = GetEntityForwardVector(PlayerPedId())
+    local forwardCoords = coords + forward * 2.0
+
+    -- Get ground Z
+    local _ , groundZ = GetGroundZFor_3dCoord(forwardCoords.x, forwardCoords.y, forwardCoords.z, false)
+
+    -- Create object
+    local obj = CreateObject('prop_protest_table_01', forwardCoords.x, forwardCoords.y, groundZ, true, true, true)
+    FreezeEntityPosition(obj, true)
     SetEntityAsMissionEntity(obj, true, true)
 
-    local target = exports.it_bridge:CreateTargetModel('prop_protest_table_01', {
-        {
-            label = 'Test',
-            name = 'bridge_tester_test',
-            icon = 'fas fa-clipboard',
-            canOxInteract = function(entity, distance, coords, name, bone)
-                return true
-            end,
-            canQbInteract = function(entity, distance, data)
-                return true
-            end,
-            onOxSelect = function(data)
-                print('Test OX')
-            end,
-            onQbInteract = function(entity)
-                print('Test QB')
-            end,
-            distance = 1.5
-        },
-        {
-            label = 'Remove Model',
-            name = 'bridge_tester_remove',
-            icon = 'fas fa-clipboard',
-            canOxInteract = function(entity, distance, coords, name, bone)
-                return true
-            end,
-            canQbInteract = function(entity, distance, data)
-                return true
-            end,
-            onOxSelect = function(data)
-                print('Remove OX')
-            end,
-            onQbInteract = function(entity)
-                DeleteEntity(entity)
-            end,
-            distance = 1.5
-        }
-    })
+    local objCoords = GetEntityCoords(obj)
+    local objHeading = GetEntityHeading(obj)
 
-    Wait(10000)
+    if exports.it_bridge:GetServerInteraction() == 'qb-target' then
+        objHeading = objHeading + 90.0
+    end
+    -- Create target
+    local boxData = {
+        id = 'bridge_tester_zone_test',
+        coords = vector3(objCoords.x, objCoords.y, objCoords.z),
+        rotation = objHeading,
+        size = size, --extendedTableData.target.size or vector3(1.0, 1.0, 1.0),
+        drawSprite = true,
+        interactDistance = 1.5,
+        minZ = objCoords.z - 0.5,
+        maxZ = objCoords.z + (size.z / 2),
+        debug = true
+    }
 
-    exports.it_bridge:RemoveTargetModel('prop_protest_table_01')
+    local target = exports.it_bridge:CreateBoxZone(boxData, options)
+
+    Wait(15000)
+
+    exports.it_bridge:RemoveBoxZone(target)
+    DeleteEntity(obj)
     SetModelAsNoLongerNeeded('prop_protest_table_01')
 end)
 
+RegisterNetEvent('bridge_tester:client:targetEntityTest', function()
+    lib.requestModel(model)
+    -- Get coords infront of player
+    local coords = GetEntityCoords(PlayerPedId())
+    local forward = GetEntityForwardVector(PlayerPedId())
+    local forwardCoords = coords + forward * 2.0
+
+    -- Get ground Z
+    local _ , groundZ = GetGroundZFor_3dCoord(forwardCoords.x, forwardCoords.y, forwardCoords.z, false)
+
+    -- Create object
+    local obj = CreateObject('prop_protest_table_01', forwardCoords.x, forwardCoords.y, groundZ, true, true, true)
+    SetEntityAsMissionEntity(obj, true, true)
+
+    -- Get NetID
+    local netId = NetworkGetNetworkIdFromEntity(obj)
+
+    -- Create target
+    local createdOptions = exports.it_bridge:AddTargetEntity(netId, options)
+
+    Wait(15000)
+
+    exports.it_bridge:RemoveTargetEntity(netId, createdOptions)
+
+    -- Delete object
+    DeleteEntity(obj)
+    SetModelAsNoLongerNeeded('prop_protest_table_01')
+end)
 
 RegisterNetEvent('bridge_tester:client:globalPedTest', function()
-    local target = exports.it_bridge:CreateGlobalPed({
-        {
-            label = 'Test',
-            name = 'bridge_tester_test',
-            icon = 'fas fa-clipboard',
-            canOxInteract = function(entity, distance, coords, name, bone)
-                return true
-            end,
-            canQbInteract = function(entity, distance, data)
-                return true
-            end,
-            onOxSelect = function(data)
-                print('Test OX')
-            end,
-            onQbInteract = function(entity)
-                print('Test QB')
-            end,
-            distance = 1.5
-        },
-    })
 
-    Wait(10000)
+    local createdOptions = exports.it_bridge:AddGlobalPed(options)
 
-    exports.it_bridge:RemoveGlobalPed(target)
+    Wait(15000)
+
+    exports.it_bridge:RemoveGlobalPed(createdOptions)
+end)
+
+RegisterNetEvent('bridge_tester:client:globalPlayerTest', function()
+
+    local createdOptions = exports.it_bridge:AddGlobalPlayer(options)
+
+    Wait(15000)
+
+    exports.it_bridge:RemoveGlobalPlayer(createdOptions)
+end)
+
+RegisterNetEvent('bridge_tester:client:globalVehicleTest', function()
+
+    local createdOptions = exports.it_bridge:AddGlobalVehicle(options)
+
+    Wait(15000)
+
+    exports.it_bridge:RemoveGlobalVehicle(createdOptions)
+end)
+
+RegisterNetEvent('bridge_tester:client:modelTest', function()
+    lib.requestModel(model)
+    -- Get coords infront of player
+    local coords = GetEntityCoords(PlayerPedId())
+    local forward = GetEntityForwardVector(PlayerPedId())
+    local forwardCoords = coords + forward * 2.0
+
+    -- Get ground Z
+    local _ , groundZ = GetGroundZFor_3dCoord(forwardCoords.x, forwardCoords.y, forwardCoords.z, false)
+
+    -- Create object
+    local obj = CreateObject('prop_protest_table_01', forwardCoords.x, forwardCoords.y, groundZ, true, true, true)
+    SetEntityAsMissionEntity(obj, true, true)
+
+    -- Create target
+    local createdOptions = exports.it_bridge:AddTargetModel('prop_protest_table_01', options)
+
+    Wait(15000)
+
+    exports.it_bridge:RemoveTargetModel('prop_protest_table_01', createdOptions)
+
+    -- Delete object
+    DeleteEntity(obj)
+    SetModelAsNoLongerNeeded('prop_protest_table_01')
 end)
--- bridge_tester/server/targetTest.lua
+++ bridge_tester/server/targetTest.lua
@@ -1,4 +1,4 @@
-lib.addCommand('bridgeTestModelTarget', {
+lib.addCommand('bridgeBoxZone', {
     help = 'Test Bridge target export',
     params = {
         {
@@ -15,11 +15,32 @@ lib.addCommand('bridgeTestModelTarget', {
         return
     end
 
-
-    TriggerClientEvent('bridge_tester:client:modelTargetTest', target)
+    TriggerClientEvent('bridge_tester:client:targetZoneTest', target)
+    it.print.info('Spawned new target zone in front of the player', target, 'for the next 15 seconds')
 end)
 
-lib.addCommand('bridgeTestGlobalped', {
+lib.addCommand('targetEntity', {
+    help = 'Test Bridge target export',
+    params = {
+        {
+            name = 'target',
+            type = 'playerId',
+            help = 'Target player\'s server id',
+        },
+    },
+}, function(source, args, raw)
+    local target = args.target
+
+    if source ~= 0 then
+        lib.print.error('This command can only be executed from the server console')
+        return
+    end
+
+    TriggerClientEvent('bridge_tester:client:targetEntityTest', target)
+    it.print.info('Spawned new target entity in front of the player', target, 'for the next 15 seconds')
+end)
+
+lib.addCommand('globalPedTest', {
     help = 'Test Bridge target export',
     params = {
         {
@@ -37,4 +58,68 @@ lib.addCommand('bridgeTestGlobalped', {
     end
 
     TriggerClientEvent('bridge_tester:client:globalPedTest', target)
+    it.print.info('Created a global ped target for the player', target, 'for the next 15 seconds (Interact was added to every ped)')
+end)
+
+lib.addCommand('globalPlayer', {
+    help = 'Test Bridge target export',
+    params = {
+        {
+            name = 'target',
+            type = 'playerId',
+            help = 'Target player\'s server id',
+        },
+    },
+}, function(source, args, raw)
+    local target = args.target
+
+    if source ~= 0 then
+        lib.print.error('This command can only be executed from the server console')
+        return
+    end
+
+    TriggerClientEvent('bridge_tester:client:globalPlayerTest', target)
+    it.print.info('Created a global player target for the player', target, 'for the next 15 seconds (Interact was added to every player)')
+end)
+
+lib.addCommand('globalVehicleTest', {
+    help = 'Test Bridge target export',
+    params = {
+        {
+            name = 'target',
+            type = 'playerId',
+            help = 'Target player\'s server id',
+        },
+    },
+}, function(source, args, raw)
+    local target = args.target
+
+    if source ~= 0 then
+        lib.print.error('This command can only be executed from the server console')
+        return
+    end
+
+    TriggerClientEvent('bridge_tester:client:globalVehicleTest', target)
+    it.print.info('Created a global vehicle target for the player', target, 'for the next 15 seconds (Interact was added to every vehicle)')
+end)
+
+lib.addCommand('modelTest', {
+    help = 'Test Bridge target export',
+    params = {
+        {
+            name = 'target',
+            type = 'playerId',
+            help = 'Target player\'s server id',
+        },
+    },
+}, function(source, args, raw)
+    local target = args.target
+
+    if source ~= 0 then
+        lib.print.error('This command can only be executed from the server console')
+        return
+    end
+
+    TriggerClientEvent('bridge_tester:client:modelTest', target)
+    it.print.info('Spawned new target model in front of the player', target, 'for the next 15 seconds')
 end)
