48b3b5cbcd51f20f752ffc4b0984b0cb5b53db1a
Jon <unseldjonas0@gmail.com>
2025-03-02 23:57:41 +0100

    Fix textui issues

--- it_bridge/modules/textui/client.lua
+++ it_bridge/modules/textui/client.lua
@@ -11,25 +11,28 @@ function it.showTextUI(text, options)
             icon = options.icon,
             iconAnimation = options.iconAnimation,
         })
-
+        return true
     end
 
     if it.textui == TextUI.QBCORE then
         exports['qb-core']:DrawText(text, options.position)
+        return true
     end
 
     if it.textui == TextUI.ESX then
         CoreObject.TextUI(text, options.type)
-    
+        return true
     end
 
     if it.textui == TextUI.OKOK then
 
-        local okokColor = options.color == 'success' and 'darkgreen' or options.color == 'info' and 'darkblue' or options.color == 'error' and 'darkred' or 'darkgrey'
+        local okokColor = options.type == 'success' and 'darkgreen' or options.type == 'info' and 'darkblue' or options.type == 'error' and 'darkred' or 'darkgrey'
         exports['okokTextUI']:Open(text, okokColor, options.position, options.playSound)
+        return true
     end
 
     it.print.error('[showTextUI] - There was an issue showing the text UI.')
+    return false
 end
 
 function it.closeTextUI(text)
@@ -42,24 +45,29 @@ function it.closeTextUI(text)
         local isOpen, uiText = lib.isTextUIOpen()
         if isOpen then
             if uiText == text then
-                lib.closeTextUI()
+                lib.hideTextUI()
+                return true
             end
         end
     end
 
     if it.textui == TextUI.QBCORE then
         exports['qb-core']:HideText()
+        return true
     end
 
     if it.textui == TextUI.ESX then
         CoreObject.HideUI()
+        return true
     end
 
     if it.textui == TextUI.OKOK then
         exports['okokTextUI']:Close()
+        return true
     end
 
     it.print.error('[closeTextUI] - There was an issue closing the text UI.')
+    return false
 end
 
 exports('ShowTextUI', function(text, options)
